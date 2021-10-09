3f40062d2102c80e4e508bc65ea60fd0b67c7802
Pranav-Bobde <bobdep31@gmail.com>
2021-10-09 15:38:29 +0530

    Project Completed.

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var amount = document.getElementById("principal").value;
+
+    if(amount <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+  
+
+    var rateval = document.getElementById("rate").value; 
+
+    var yr = document.getElementById("years").value;
+
+    var simpleInterest = amount*rateval*yr/100;
+
+    if(amount > 0) {
+        document.getElementById("amt").innerHTML = amount;
+        document.getElementById("rateOfInterest").innerHTML = rateval;
+        document.getElementById("final").innerHTML = simpleInterest;
+        document.getElementById("year").innerHTML = new Date().getFullYear()+parseInt(yr);
+    }
     
 }
         
